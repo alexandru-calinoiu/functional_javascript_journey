e04f739ef68913c7d55a9fccb37e6300a761bba1
calin <calinoiu.alexandru@agilefreaks.com>
2015-10-12 22:07:18 +0300

    oop

---
+++ 02oop.js
@@ -0,0 +1,14 @@
+var numbers = [3, 1, 7];
+var constant = 2;
+
+function mul(a, b) { 
+	return a * b;
+}
+
+function print(n) {
+	console.log(n);
+}
+
+numbers.map(function (n) {
+	return mul(n, constant)
+}).forEach(print)
